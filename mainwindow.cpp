f8c5a6f54c74da12747bdd5cd41d334430534a2e
Ivan Komissarov <abbapoh@gmail.com>
2019-12-25 17:34:36 +0300

    Display all products

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,54 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtCore/QDir>
 #include <QtCore/QFileInfo>
+#include <QtCore/QJsonArray>
 #include <QtCore/QJsonObject>
+#include <QtCore/QStringListModel>
+
+static QStringList getProducts(const QJsonObject &project)
+{
+    QStringList list;
+    const auto products = project["products"].toArray();
+    for (const auto &product: products)
+        list.append(product["name"].toString());
+
+    const auto subProjects = project["sub-projects"].toArray();
+    for (const auto &subProject: subProjects)
+        list.append(getProducts(subProject.toObject()));
+    return list;
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    const auto model = new QStringListModel(ui->treeView);
+    ui->treeView->setModel(model);
 
     const auto dir = QStringLiteral("/Users/abbapoh/Programming/qt5/alien/qbs/build");
     const auto projectFilePath = QStringLiteral("/Users/abbapoh/Programming/qt5/alien/qbs/qbs.qbs");
+
+    const auto onProjectResolved = [this, model, dir](const ErrorInfo &error)
+    {
+        qDebug() << "project resolved";
+        if (error.hasError()) {
+            qWarning() << error.toString();
+            return;
+        }
+
+        qDebug() << m_session->projectData().keys();
+        qDebug() << m_session->projectData()["build-system-files"].toArray();
+        qDebug() << m_session->projectData()["products"].toArray();
+        QDir d(dir);
+        d.cdUp();
+        d.cd("qbs");
+        QStringList list = getProducts(m_session->projectData());
+        model->setStringList(list);
+    };
+    connect(m_session.get(), &QbsSession::projectResolved, this, onProjectResolved);
 
     QJsonObject request;
     request.insert("type", "resolve-project");
